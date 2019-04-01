f67d2c560a90d0e124ef568c6fcac89ae5d7d617
nikitsoni <47641941+nikitsoni@users.noreply.github.com>
2019-04-01 23:16:40 +0530

    first commit

---
+++ src/Card.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './Card.css'
+
+const Card = ({id, name, email}) => {
+	return(
+		<div className='tc bg-light-green dib pa3 br3 ma2 grow bw2 shadow-5'>
+			<img alt='robots' src={`https://robohash.org/${id}`} />
+			<div>
+				<h2>{name}</h2>
+				<p>{email}</p>
+			</div>
+		</div>
+		)
+}
+
+export default Card;
