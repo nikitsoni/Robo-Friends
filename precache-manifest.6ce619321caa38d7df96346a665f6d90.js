e7e048a5f8982ac90c4b90d58545f5b453523faf
nikitsoni <47641941+nikitsoni@users.noreply.github.com>
2019-04-01 23:31:08 +0530

    Updates

---
+++ precache-manifest.6ce619321caa38d7df96346a665f6d90.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "c7980f8b10c6ecf9aa29",
+    "url": "/Robo-Friends/static/js/runtime~main.a23543f7.js"
+  },
+  {
+    "revision": "13b492156598797275c4",
+    "url": "/Robo-Friends/static/js/main.d41da29e.chunk.js"
+  },
+  {
+    "revision": "d42863e988da09c39a6f",
+    "url": "/Robo-Friends/static/js/2.9051aa8e.chunk.js"
+  },
+  {
+    "revision": "13b492156598797275c4",
+    "url": "/Robo-Friends/static/css/main.63d1b332.chunk.css"
+  },
+  {
+    "revision": "d42863e988da09c39a6f",
+    "url": "/Robo-Friends/static/css/2.5f464b43.chunk.css"
+  },
+  {
+    "revision": "c2ba7e740fbc0eb1093c5c7baf2cf3bb",
+    "url": "/Robo-Friends/index.html"
+  }
+];
